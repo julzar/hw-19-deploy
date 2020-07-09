e8a3304ce82ef955a1904e4e0f21fd652f69543e
juz <electronicallyreached@gmail.com>
2020-07-08 20:17:47 -0500

    Updates

---
+++ precache-manifest.5e19455f7410f163cfc9bc38e7ff241c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d838915b9c45c98937f89cb5a2497cf1",
+    "url": "/hw-19-deploy/index.html"
+  },
+  {
+    "revision": "4768b0cbc67854a59aa7",
+    "url": "/hw-19-deploy/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4768b0cbc67854a59aa7",
+    "url": "/hw-19-deploy/static/js/2.5a226ea6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hw-19-deploy/static/js/2.5a226ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e9c448d5f9d3462141",
+    "url": "/hw-19-deploy/static/js/main.643c72f9.chunk.js"
+  },
+  {
+    "revision": "880cb45d8a4616302c16",
+    "url": "/hw-19-deploy/static/js/runtime-main.3a290682.js"
+  }
+]);
